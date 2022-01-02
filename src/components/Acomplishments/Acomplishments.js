db7f03f08138fc20f3733c9f173d841da5c76d7a
marksikaundi <emmanuelmark117@gmail.com>
2022-01-02 09:52:07 +0200

    accomplished updated

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,10 +4,10 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
+  { number: 60, text: 'Projects'},
   { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 190, text: 'Github Followers', },
+  { number: 60, text: 'Github Stars', }
 ];
 
 const Acomplishments = () => (
